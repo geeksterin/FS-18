ee7b0cf9a5ebc58ce80b8ffbd70ea55473858dc6
Aman Srivastava <aman4863@gmail.com>
2023-10-12 22:07:02 +0530

    added

---
+++ lec004/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String [] args){
+        int x = 4;
+
+        if(x == 2){
+            System.out.println("Two");
+        }
+        else if(x == 3){
+            System.out.println("Three");
+        }
+        else if(x == 4){
+            System.out.println("Four");
+        }
+        else if(x == 5){
+            System.out.println("Five");
+        }
+        else{
+            System.out.println("Not Two and Three");
+        }
+
+    }
+}
